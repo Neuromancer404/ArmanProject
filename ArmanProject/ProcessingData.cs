8c3e0904de5b3fa8c1fd2a35f96a00a7f7693db2
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2021-06-24 22:22:26 +0300

    fuck it

---
+++ ArmanProject/ProcessingData.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArmanProject
+{
+    public partial class SettingsWindow
+    {
+        private void ListBoxFilling()
+        {
+            foreach (KeyValuePair<string, SubscriberData> item in gData)
+            {
+                KeySelectionListBox.Items.Add(item.Key);
+                Console.WriteLine("--------Listbox fill-----------");
+                Console.WriteLine("{0}", item.Key);
+            }
+        }
+
+        private void FormFilling(string key)
+        {
+            SubNumLabel.Content = gData[key].SubNumber;
+            EventIdLabel.Content = gData[key].eventId;
+            KeyNumLabel.Content = gData[key].value_key;
+            SubNameTextBox.Text = gData[key].SubName;
+            DiscriptTextBox.Text = gData[key].Discript;
+            MacTextBox.Text = gData[key].MAC;
+            IpTextBox.Text = gData[key].IP;
+            ValueVisibleCheckBox.IsChecked = gData[key].value_visible;
+            Console.WriteLine("gData value writing");
+        }
+
+        private void atStart()
+        {
+            Console.WriteLine("atStart starting");
+
+            string[] filesName = FileExtensionCheking(pathToParameterFilesFolder);
+            if (filesName.Length == 0)
+            {
+                Console.WriteLine("Error: no .par in {0}", pathToParameterFilesFolder);
+                return;
+            }
+
+            gData.Clear();
+
+            foreach (string pathToFile in filesName)
+            {
+                parseFile(pathToFile);
+            }
+
+            foreach (KeyValuePair<string, SubscriberData> kvp in gData)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value.eventId);
+            }
+
+            ListBoxFilling();
+        }
+
+        private string[] FileExtensionCheking(string path)
+        {
+            string[] pathToParFiles = System.IO.Directory.GetFiles(path, "*.par");
+
+            return pathToParFiles;
+        }
+
+        public void parseFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string sub = "";
+
+            foreach (string par in lines)
+            {
+                if (par.Contains("par11"))
+                {
+                    sub = getSub(par);
+                }
+                if (par.Contains("par24") && sub.Length > 0)
+                {
+                    SubscriberData sd = new SubscriberData();
+                    if (checkEvent(par, sd))
+                    {
+                        sd.SubNumber = sub;
+                        string key = sd.SubNumber + "_" + sd.eventId + "_" + sd.value_key;
+                        Console.WriteLine(key);
+                        gData.Add(key, sd);
+                    }
+                }
+            }
+        }
+
+
+        private bool checkEvent(string parameterData, SubscriberData _subscriberData)
+        {
+            if (parameterData.Contains("eventrecv"))
+            {
+                _subscriberData.value_key = getButton(parameterData);
+                _subscriberData.eventId = getEvent(parameterData);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public string getButton(string par)
+        {
+            string str = par.Substring(6, par.Length - 6);
+            int a = str.IndexOf(":");
+            str = str.Substring(0, a);
+            Console.WriteLine("button number = {0}", str);
+
+            return str;
+        }
+
+        private string getEvent(string par)
+        {
+            string[] container;
+
+            int b = par.IndexOf("\"");
+            string str = par.Substring(b + 1);
+
+            b = str.IndexOf("\"");
+            str = str.Substring(0, b);
+
+
+            container = str.Split('|', '&');
+            foreach (string str1 in container)
+            {
+                Console.WriteLine("events = {0}", str1);
+            }
+            Console.WriteLine("=============================");
+
+            return container[0];
+
+        }
+
+        private string getSub(string parameterData)
+        {
+            string retVal = "";
+            string[] container;
+            string a;
+
+            a = parameterData.Substring(3);
+            container = a.Split(':', '@');
+
+            retVal = container[1];
+
+            Console.WriteLine("===={0}", retVal);
+
+            return retVal;
+        }
+        /// <summary>
+        /// При выборе значения из ListBox вызывает метод заполнения формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void KeySelectionListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FormFilling(KeySelectionListBox.SelectedItem.ToString());
+        }
+
+    }
+}
